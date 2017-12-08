7953c03e15affb05876770169a6d9c4714038cac
Pawel Grochola <grocholapawel@gmail.com>
2017-12-08 13:27:27 +0100

    Add pushing error when frame is not read

--- rxpy/frame_observable.py
+++ rxpy/frame_observable.py
@@ -4,7 +4,9 @@ import cv2
 def observable_frames_cv2(observer):
     cap = cv2.VideoCapture(0)
     while True:
-        _, frame = cap.read()
+        ret, frame = cap.read()
+        if not ret:
+            observer.on_error("No camera feed")
         frame = cv2.flip(frame, 1)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         observer.on_next(cv2image)
