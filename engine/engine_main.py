c5072573c8a9d5c209ac6bb9cf23277ac85b1bf6
Pawel Grochola <grocholapawel@gmail.com>
2017-12-19 19:15:59 +0100

    Make function for finger counting

--- engine/engine_main.py
+++ engine/engine_main.py
@@ -1,9 +1,12 @@
 import time
-from image_processors import ImagePipeline
-
+from image_processors import ImagePipeline, ImageProcessor
+from .finger.fingers.finger_processor import finger_processor
 
 def main():
     pipeline = ImagePipeline()
+    pipeline.run()
+    time.sleep(3)
+    pipeline.replace_processor(ImageProcessor(finger_processor))
     pipeline.run()
     time.sleep(3)
     pipeline.stop_all()
