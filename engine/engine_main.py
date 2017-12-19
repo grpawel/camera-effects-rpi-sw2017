7347e3c4ecdaeb7e592258357738fe87d7dcecc3
Pawel Grochola <grocholapawel@gmail.com>
2017-12-19 18:38:02 +0100

    Implement ImageProcessor holding queues

--- engine/engine_main.py
+++ engine/engine_main.py
@@ -4,53 +4,65 @@ from queue import Queue
 
 import cv2
 
-input_frames_queue = Queue(10)
-out_put_frames_queue = Queue(10)
-
 
 class ImageGrabber(threading.Thread):
-    def __init__(self):
+    def __init__(self, input_frames_queue):
         threading.Thread.__init__(self)
         self.cam = cv2.VideoCapture(0)
+        self.input_frames_queue = input_frames_queue
 
     def run(self):
-        global input_frames_queue
         while True:
             ret, frame = self.cam.read()
-            input_frames_queue.put(frame)
+            self.input_frames_queue.put(frame)
 
 
 class ImageDisplayer(threading.Thread):
-    def __init__(self):
+    def __init__(self, output_frames_queue):
         threading.Thread.__init__(self)
+        self.output_frames_queue = output_frames_queue
 
     def run(self):
-        global out_put_frames_queue
         while True:
-            if not out_put_frames_queue.empty():
-                f = out_put_frames_queue.get()
-                out_put_frames_queue.task_done()
+            if not self.output_frames_queue.empty():
+                f = self.output_frames_queue.get()
+                self.output_frames_queue.task_done()
                 cv2.imshow('frame', f)
                 key = cv2.waitKey(1) & 0xFF
 
+
 class ImageProcessor(threading.Thread):
-    def __init__(self):
+    def __init__(self, input_frames_queue, output_frames_queue):
         threading.Thread.__init__(self)
+        self.input_frames_queue = input_frames_queue
+        self.output_frames_queue = output_frames_queue
 
     def run(self):
-        global input_frames_queue
-        global out_put_frames_queue
         while True:
-            if not input_frames_queue.empty():
-                f = input_frames_queue.get()
-                input_frames_queue.task_done()
-                out_put_frames_queue.put(f)
+            if not self.input_frames_queue.empty():
+                f = self.input_frames_queue.get()
+                self.input_frames_queue.task_done()
+                self.output_frames_queue.put(f)
+
+
+class ImagePipeline():
+    def __init__(self):
+        self.output_frames_queue = Queue(10)
+        self.input_frames_queue = Queue(10)
+        self.grabber = ImageGrabber(self.input_frames_queue)
+        self.displayer = ImageDisplayer(self.output_frames_queue)
+        self.processor = ImageProcessor(self.input_frames_queue, self.output_frames_queue)
+
+    def run(self):
+        self.grabber.start()
+        self.displayer.start()
+        self.processor.start()
+
+
+def main():
+    pipeline = ImagePipeline()
+    pipeline.run()
 
 
 if __name__ == '__main__':
-    grabber = ImageGrabber()
-    displayer = ImageDisplayer()
-    processer = ImageProcessor()
-    grabber.start()
-    displayer.start()
-    processer.start()
+    main()
