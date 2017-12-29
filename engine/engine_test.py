c112aacf58308f90ddf78d741a0721eb8816a8ee
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2017-12-29 21:49:13 +0100

    multithread test

--- engine/engine_test.py
+++ engine/engine_test.py
@@ -1,12 +1,40 @@
-import cv2
-from numpy.matlib import eye
 
-cap = cv2.VideoCapture(0)
-from engine.edges.edge_processor import edge_processor
+from threading import Thread
+import cv2
+
 from engine.face_filters.face_detector import face_detector
 
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+
+        while True:
+            if self.stopped:
+                return
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
+
+vs = WebcamVideoStream(src=0).start()
+
 while True:
-    _,frame = cap.read()
+
+
+
+    frame = vs.read()
     im = face_detector(frame, faces=True, mouths=True)
-    cv2.imshow('frame', im)
+    cv2.imshow("Frame", im)
     key = cv2.waitKey(1) & 0xFF
