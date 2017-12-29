61035ebd8d77a918b7ad483c48a49b32023756e5
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2017-12-29 12:16:46 +0100

    Add simple example

--- engine/engine_test.py
+++ engine/engine_test.py
@@ -1,9 +1,10 @@
 import cv2
 cap = cv2.VideoCapture(0)
 from engine.edges.edge_processor import edge_processor
+from engine.face_filters.face_detector import face_detector
 
 while True:
     _,frame = cap.read()
-    im = edge_processor(frame)
+    im = face_detector(frame)
     cv2.imshow('frame', im)
     key = cv2.waitKey(1) & 0xFF
