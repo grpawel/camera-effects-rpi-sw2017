e7f775be1518eb56a782752f8ef59a2f426bc8b6
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2017-12-29 12:15:13 +0100

    Removal of unnecesary print

--- engine/face_filters/face_detector.py
+++ engine/face_filters/face_detector.py
@@ -10,10 +10,7 @@ import numpy as np
 def face_detector(img):
     scale = 1.3
     min_neighbours = 5
-    files = [f for f in os.listdir('..') if os.path.isfile(f)]
-    for f in files:
-        print(f)
-    #load faces and eyes definition
+    #load faces  definition
     face_cascade = cv2.CascadeClassifier('./res/haarcascades/haarcascade_frontalface_default.xml')
     # convert to gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
