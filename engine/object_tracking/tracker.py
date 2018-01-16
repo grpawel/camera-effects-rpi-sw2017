b8cb4bda54874d0ff7dee9563d0ef5704024cc9f
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-01-16 19:02:19 +0100

    Add non-sequentional frame processing

--- engine/object_tracking/tracker.py
+++ engine/object_tracking/tracker.py
@@ -15,42 +15,43 @@ class ObjectTracker():
         self.x = None
         self.y = None
         self.radius = None
+        self.resize_scale = 0.3
 
 
     def process(self, img):
 
-        frame = imutils.resize(img, width=600)
+        working_frame = cv2.resize(img,None,fx=self.resize_scale, fy=self.resize_scale)
         if self.trace_live > 0:
-            cv2.circle(frame, (int(self.x), int(self.y)), int(self.radius),
+            cv2.circle(img, (int(self.x), int(self.y)), int(self.radius),
                        (0, 255, 255), 2)
-            cv2.circle(frame, self.center, 5, (0, 0, 255), -1)
+            cv2.circle(img, self.center, 5, (0, 0, 255), -1)
             trace_size = 10
             traces = self.centres[-(self.TRACES - self.trace_start):] + self.centres[:self.trace_start]
             for trace in reversed(traces):
                 trace_size = int(0.98 * trace_size)
-                cv2.circle(frame, trace, trace_size, (0, 0, 255), -1)
+                cv2.circle(img, trace, trace_size, (0, 0, 255), -1)
             self.trace_live-=1
         else:
-            cv2.GaussianBlur(frame, (11, 11), 0)
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            cv2.GaussianBlur(working_frame, (11, 11), 0)
+            hsv = cv2.cvtColor(working_frame, cv2.COLOR_BGR2HSV)
             mask = self.build_mask(hsv)
             counturs = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
             if len(counturs) > 0:
                 c = max(counturs, key=cv2.contourArea)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
-                self.x = x
-                self.y = y
-                self.radius = radius
+                self.x = int(x/self.resize_scale)
+                self.y = int(y/self.resize_scale)
+                self.radius = int(radius/self.resize_scale)
                 M = cv2.moments(c)
-                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                center = tuple(map(lambda x: int(x/self.resize_scale),(int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))))
                 self.center = center
                 self.centres[self.trace_start] = center
                 self.trace_start = (self.trace_start + 1) % self.TRACES
 
                 self.trace_live = 5
 
-        return 'Tracker', frame
+        return 'Tracker', img
 
     def build_mask(self, hsv):
         mask = cv2.inRange(hsv, self.object_colour_rgb_bound_down, self.object_colour_rgb_bound_up)
