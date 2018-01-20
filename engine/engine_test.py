d9a066fbf054fef3f3e88edd77ed9d4d86821b2b
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-01-20 21:50:03 +0100

    runtime function param change

--- engine/engine_test.py
+++ engine/engine_test.py
@@ -18,7 +18,8 @@ ob = ObjectTracker(np.array([30, 150, 50]), np.array([255, 255, 255]))
 inv = Inverter()
 nr = NoiseRemover()
 # list of image processing function, and change param func
-funs = [(edge_processor, None), (fd.procces_img, fd.next), fa.draw_moustache, fa.draw_hat, (ob.process, ob.next), finger_processor, inv.invert, nr.remove_noise]
+funs = [(edge_processor, None), (fd.procces_img, fd.next), (fa.draw_moustache, None), (fa.draw_hat, None),
+        (ob.process, ob.next), (finger_processor, None), (inv.invert, None), (nr.remove_noise, None)]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -40,14 +41,16 @@ def change_func(_):
         fun = funs[i%f]
         last_change = current_time
         fun_changed = True
-def exit_func(_):
-    exit()
+def change_param(_):
+    if fun[1] is not None:
+        fun[1]()
+
     
 GPIO.add_event_detect(gpio_pin, GPIO.FALLING, callback=change_func)
-GPIO.add_event_detect(18, GPIO.FALLING, callback=exit_func)
+GPIO.add_event_detect(18, GPIO.FALLING, callback=change_param)
 while True:
     _,frame = vs.read()
-    name, im = fun(frame)
+    name, im = fun[0](frame)
     cv2.imshow(name, im)
     key = cv2.waitKey(1) & 0xFF
     if fun_changed:
