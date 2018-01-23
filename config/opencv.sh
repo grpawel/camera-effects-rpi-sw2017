9f38fba9a9c7c988b0804204e6fd1268f210fda3
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-01-23 14:58:08 +0100

    fix script

--- config/opencv.sh
+++ config/opencv.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-PYTHON_VERSION='3.5'
-OPEN_CV_VERSION='3.3.1'
+PYTHON_VERSION=3.5
+OPEN_CV_VERSION=3.3.1
+RPI_CAMERA_EFFECTS=/home/pi/projects/sw
 
 sudo apt-get purge wolfram-engine
 sudo apt-get purge libreoffice*
@@ -13,6 +14,7 @@ sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev
 sudo apt-get install libgtk2.0-dev libgtk-3-dev
 sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install fswebcam
 sudo python3 get-pip.py
 sudo pip3 install virtualenv virtualenvwrapper
 sudo pip3 install numpy
@@ -42,9 +44,13 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$OPEN_CV_VERSION/modules \
     -D BUILD_EXAMPLES=ON ..
 
-make -j4
+sudo make -j4
 sudo make install
 sudo ldconfig
 sudo mv /usr/local/lib/python$PYTHON_VERSION/site-packages/cv2.cpython-35m-arm-linux-gnueabihf.so /usr/local/lib/python$PYTHON_VERSION/site-packages/cv2.so
 ln -s /usr/local/lib/python$PYTHON_VERSION/site-packages/cv2.so /usr/local/lib/python$PYTHON_VERSION/site-packages/cv2.so
+echo @lxterminal --command $RPI_CAMERA_EFFECTS/camera-effects-rpi-sw2017/engine/start.sh >> ~/.config/lxsession/LXDE-pi/autostart
+echo sudo reboot > /home/pi/Desktop/power-off.sh
+chmod u+x /home/pi/Desktop/power-off.sh
+
 sudo reboot
